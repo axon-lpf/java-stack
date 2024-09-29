a31edc3f985a099722c0aa4bcec44460eb302b20
rainbow-lpf <rainbow-lpf@outlook.com>
2024-09-29 18:37:07 +0800

    添加八皇后打印

--- src/main/java/com/axon/java/stack/data/structures/stack/eight/queens/EightQueens2.java
+++ src/main/java/com/axon/java/stack/data/structures/stack/eight/queens/EightQueens2.java
@@ -1,5 +1,7 @@
 package com.axon.java.stack.data.structures.stack.eight.queens;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class EightQueens2 {
 
     private static final int N = 8; // 棋盘的大小 (8x8)
@@ -21,19 +23,36 @@ public class EightQueens2 {
             //printSolution();
             return;
         }
-
         // 尝试在当前行的每一列放置皇后
+        // 7,0=>7,1=>7,2=>7,3=>7,4=>7,5=>7,6=>7,7
+        //遍历列, 判断当前的行，列与之前的是否冲突。
         for (int col = 0; col < N; col++) {
-            if (isSafe(row, col)) {
-                board[row] = col; // 将皇后放置在当前列
-                solve(row + 1);   // 递归尝试下一行
-                // 回溯，不需要手动重置 board[row]，因为在下一次递归时会覆盖
+            //默认true，表名不在同一列且同一对角线上， false则说明在
+            boolean flag = true;
+            //遍历行
+            for (int tempRow = 0; tempRow < row; tempRow++) {
+
+                // 检查同一列是否已有皇后
+                if (board[tempRow] == col) {
+                    flag = false;
+                }
+                // 检查是否在同一对角线上
+                if (Math.abs(board[tempRow] - col) == Math.abs(tempRow - row)) {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                // 将皇后放置在当前列
+                board[row] = col;
+                // 递归尝试下一行
+                solve(row + 1);
             }
         }
     }
 
     // 检查在 (row, col) 位置放置皇后是否安全
     public static boolean isSafe(int row, int col) {
+        //这里又从零行去循环遍历，检查与当前列的值，是否在同一列或者同一对角线上
         for (int x = 0; x < row; x++) {
             // 检查同一列是否已有皇后
             if (board[x] == col) {
@@ -63,7 +82,7 @@ public class EightQueens2 {
         System.out.println("解法 " + solutionCount + ":");
         // 打印每个皇后的坐标位置
         for (int row = 0; row < N; row++) {
-            System.out.print("皇后" + (row + 1) + "： (行, 列) = (" + (row + 1) + ", " + (board[row] + 1) + ")");
+            System.out.print("皇后" + (row) + "： (行, 列) = (" + (row) + ", " + (board[row]) + ")");
             System.out.println();
         }
         System.out.println();
