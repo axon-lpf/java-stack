0006c41e59827ed70135d9ce9e710ce09744cae3
rainbow-lpf <rainbow-lpf@outlook.com>
2024-10-28 14:00:19 +0800

    添加希尔排序算法

---
+++ src/main/java/com/axon/java/stack/algorithms/sort/shellsort/ShellSortDemo.java
@@ -0,0 +1,155 @@
+package com.axon.java.stack.algorithms.sort.shellsort;
+
+import java.util.Arrays;
+
+/**
+ * /**
+ * * 希尔排序算法
+ * * 初始化数组:  [8, 9, 1, 7, 2, 3, 5, 4, 6, 0]
+ * * 处理公式： gap=length/2
+ * * 第一轮:
+ * * 目前有10个元素， 则gap=10/2=5;  则步长为5， 这样
+ * * 进行分组： 第一组： 则从索引为0开始，步长为5，  则是 [8,3]
+ * * 第二组： 则从索引为1开始，步长为5，   则是[9,5]
+ * * 第三组： 则从
+ * * <p>
+ * * <p>
+ * * 第二轮： 则在上一轮的步长基础上再次除以2，  gap= 5/2, 则步长为2
+ * * <p>
+ * * <p>
+ * * 第三轮：在在第二轮的步长基础之上再次除以2， gap=2/2 , 则步长为1
+ */
+
+public class ShellSortDemo {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{8, 9, 1, 7, 2,
+                3, 5, 4, 6, 0};
+
+     /*   // 进行拆解处理
+        System.out.println("开始第一轮的处理");
+        step1(arr);
+        //[3, 5, 1, 6, 0, 8, 9, 7, 4, 2]
+        System.out.println("开始第二轮的处理");
+        step2(arr);
+        System.out.println("开始第三轮的处理");
+        step3(arr);
+
+        arr = new int[]{8, 9, 1, 7, 2,
+                3, 5, 4, 6, 0};
+        //交换赋值法
+        shellSort(arr);*/
+
+        arr = new int[]{8, 9, 1, 7, 2,
+                3, 5, 4, 6, 0};
+        shellSort2(arr);
+    }
+
+
+    /**
+     * 这里使用的交换赋值的方法
+     *
+     * @param arr
+     */
+    public static void shellSort(int[] arr) {
+
+        for (int gap = arr.length / 2; gap > 0; gap = gap / 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int temp = arr[i];
+                for (int j = i - gap; j >= 0; j = j - gap) {
+                    if (temp < arr[j]) {
+                        //temp 要将大于的值进行后移
+                        arr[j + gap] = arr[j];
+                        //当前的最小值
+                        arr[j] = temp;
+                        //更新最小值
+                        temp = arr[j];
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    /**
+     * 位移法 ，效率比交换赋值法更高， 交换赋值不算希尔排序，主要是方便理解的处理。
+     *
+     * @param arr
+     */
+    public static void shellSort2(int[] arr) {
+        int minIndex = 0;
+        int temp = 0;
+        for (int gap = arr.length / 2; gap > 0; gap = gap / 2) {
+            for (int i = gap; i < arr.length; i++) {
+                temp = arr[i]; //初始值 arr[5]=3;
+                minIndex = i; //初始值是5，
+                while (minIndex >= gap && temp < arr[minIndex - gap]) { //初始值  arr[5 - 5]=8    3<8
+                    arr[minIndex] = arr[minIndex - gap];  //初始值 arr[5]= arr[5-5]    arr[5]=arr[5-5]   即 arr[5]=8  这里是先把大的值赋值
+                    minIndex = minIndex - gap;  //记录最小值的位置   minIndex=5-5=0; 最小值的位置0，
+                }
+                if (minIndex != i) {
+                    arr[minIndex] = temp; // 针对这一轮的比较，最小值进行赋值
+                }
+                System.out.println(Arrays.toString(arr));
+            }
+        }
+    }
+
+
+    /**
+     * 按照插入排序算法， 第一个元素，则从第5个元素开始
+     *
+     * @param arr
+     */
+    public static void step1(int[] arr) {
+
+        for (int i = 5; i < arr.length; i++) {
+            int temp = arr[i];
+            for (int j = i - 5; j >= 0; j = j - 5) {
+                if (temp < arr[j]) {
+                    //temp 要将大于的值进行后移
+                    arr[j + 5] = arr[j];
+                    //当前的最小值
+                    arr[j] = temp;
+                    //更新最小值
+                    temp = arr[j];
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+
+    public static void step2(int[] arr) {
+
+        for (int i = 2; i < arr.length; i++) {
+            int temp = arr[i];
+            for (int j = i - 2; j >= 0; j = j - 2) {
+                if (temp < arr[j]) {
+                    arr[j + 2] = arr[j];
+                    arr[j] = temp;
+                    temp = arr[j];
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+
+    public static void step3(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            for (int j = i - 1; j >= 0; j = j - 1) {
+                if (temp < arr[j]) {
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                    temp = arr[j];
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+}
