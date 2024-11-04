768c28c62c014881ce529a055dd0397bd9d7372f
rainbow-lpf <rainbow-lpf@outlook.com>
2024-11-04 09:04:14 +0800

    修改斐波拉契算法命名

---
+++ src/main/java/com/axon/java/stack/algorithms/serach/fibonacciserach/LeonardoFibonacciDemo.java
@@ -0,0 +1,11 @@
+package com.axon.java.stack.algorithms.serach.fibonacciserach;
+
+/**
+ * 斐波拉切查找算法
+ */
+public class LeonardoFibonacciDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
