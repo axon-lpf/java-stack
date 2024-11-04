8957b5228c7a79ccb8de51898da2fc7d9d26ba38
rainbow-lpf <rainbow-lpf@outlook.com>
2024-11-04 09:04:58 +0800

    修改名称

--- src/main/java/com/axon/java/stack/algorithms/serach/fibonacciserach/FibonacciDemo.java
+++ src/main/java/com/axon/java/stack/algorithms/serach/fibonacciserach/FibonacciDemo.java
@@ -3,7 +3,7 @@ package com.axon.java.stack.algorithms.serach.fibonacciserach;
 /**
  * 斐波拉切查找算法
  */
-public class LeonardoFibonacciDemo {
+public class FibonacciDemo {
 
     public static void main(String[] args) {
 
