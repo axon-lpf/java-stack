3c706674ac3a46ffbcdd6a51bb4122fa0e921cb3
rainbow-lpf <rainbow-lpf@outlook.com>
2024-11-28 10:05:11 +0800

    图的遍历的核心代码

--- src/main/java/com/axon/java/stack/data/structures/graph/GraphDemo.java
+++ src/main/java/com/axon/java/stack/data/structures/graph/GraphDemo.java
@@ -35,6 +35,8 @@ public class GraphDemo {
 
         graph.showEdges();
 
+        graph.dfs();
+
 
 
     }
@@ -59,10 +61,16 @@ class Graph{
      */
     private ArrayList<String> graphValue;
 
+    /**
+     *  是否被访问过
+     */
+    private  boolean [] isVisited;
+
 
     public Graph(int size) {
         edges = new int[size][size];
         graphValue = new ArrayList<>();
+        isVisited =new boolean[size];
     }
 
     /**
@@ -129,5 +137,128 @@ class Graph{
         }
     }
 
+
+    /**
+     *  获取第一个相邻顶点
+     * @param index 下标位置
+     * @return
+     */
+    public  int  getFirstVertices(int index){
+        for (int i = 0; i < this.graphValue.size(); i++) {
+
+            if (this.edges[index][i]>0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     *   查询下一个相邻顶点
+     * @param v1Index  第一个顶点的位置
+     * @param v2Index  第二个顶点的位置
+     * @return
+     */
+    public  int getNextVertices(int v1Index, int v2Index){
+        for (int i = v2Index+1; i < edges.length ; i++) {
+            if (edges[v1Index][i]>0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     *  深度优先遍历
+     *
+     *       *            A   B   C   D   E
+     *      *  *      A   0   1   1   0   0
+     *      *  *      B   1   0   1   1   1
+     *      *  *      C   1   1   0   0   0
+     *      *  *      D   0   1   0   0   0
+     *      *  *      E   0   1   0   0   0
+     *
+     *
+     *  以下为代码运行的流程（按邻接矩阵和递归顺序）：
+     * 	1.	入口调用：dfs()
+     * 	•	遍历 isVisited，从第一个未访问的节点（A）开始调用 dfs(isVisited, 0)。
+     * 	2.	递归过程：
+     * 	•	dfs(isVisited, 0)：
+     * 	•	输出：当前节点被访问A。
+     * 	•	标记 A 为已访问。
+     * 	•	找到 A 的第一个相邻节点 B（下标 1）。
+     * 	•	递归调用：dfs(isVisited, 1)。
+     * 	•	dfs(isVisited, 1)：
+     * 	•	输出：当前节点被访问B。
+     * 	•	标记 B 为已访问。
+     * 	•	找到 B 的第一个相邻节点 A，但已访问，继续找下一个相邻节点 C。
+     * 	•	递归调用：dfs(isVisited, 2)。
+     * 	•	dfs(isVisited, 2)：
+     * 	•	输出：当前节点被访问C。
+     * 	•	标记 C 为已访问。
+     * 	•	找到 C 的第一个相邻节点 A，但已访问，继续找下一个相邻节点 B，但已访问。
+     * 	•	没有其他未访问的相邻节点，递归结束返回上一层。
+     * 	•	回到 dfs(isVisited, 1)：
+     * 	•	继续寻找 B 的下一个相邻节点 D。
+     * 	•	递归调用：dfs(isVisited, 3)。
+     * 	•	dfs(isVisited, 3)：
+     * 	•	输出：当前节点被访问D。
+     * 	•	标记 D 为已访问。
+     * 	•	找到 D 的相邻节点 B，但已访问。
+     * 	•	没有其他未访问的相邻节点，递归结束返回上一层。
+     * 	•	回到 dfs(isVisited, 1)：
+     * 	•	继续寻找 B 的下一个相邻节点 E。
+     * 	•	递归调用：dfs(isVisited, 4)。
+     * 	•	dfs(isVisited, 4)：
+     * 	•	输出：当前节点被访问E。
+     * 	•	标记 E 为已访问。
+     * 	•	找到 E 的相邻节点 B，但已访问。
+     * 	•	没有其他未访问的相邻节点，递归结束返回上一层。
+     * 	3.	递归结束，回到 dfs() 主循环：
+     * 	•	所有节点已访问，深度优先遍历完成。
+     */
+    public  void dfs(){
+
+        for (int i = 0; i < isVisited.length; i++) {
+            if (!isVisited[i]) {
+                dfs(isVisited,i);
+            }
+        }
+    }
+
+
+    /**
+     *
+     *  * 案例图， 图的表示连接矩阵
+     *  *          A   B   C   D   E
+     *  *      A   0   1   1   0   0
+     *  *      B   1   0   1   1   1
+     *  *      C   1   1   0   0   0
+     *  *      D   0   1   0   0   0
+     *  *      E   0   1   0   0   0
+     *
+     *
+     *
+     *
+     * @param isVisited
+     * @param i
+     */
+    public  void  dfs(boolean [] isVisited, int i){
+
+        System.out.println("当前节点被访问"+this.getVertices(i));
+        this.isVisited[i]=true;
+
+        //获取与他相邻的第一个顶点
+        int w = this.getFirstVertices(i);
+
+        while (w != -1){
+            if (!isVisited[w]){
+                dfs(isVisited,w);
+            }
+            //否则的话获取下一个顶点
+            w = this.getNextVertices(i,w);
+        }
+
+    }
 
 }
